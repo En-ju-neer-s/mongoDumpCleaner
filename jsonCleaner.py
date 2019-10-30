eb5fa6b6a7ed1e301163ce41bdb4da71db1f8466
Roel Voordendag <rvoordendag@gmail.com>
2019-10-30 22:19:23 +0100

    Co-authored-by: Mark Vonk <markjhvonk@gmail.com> Added extra checkes to make json valid

--- jsonCleaner.py
+++ jsonCleaner.py
@@ -2,10 +2,11 @@ import json
 import re
 import os
 
-directoryName = 'test'
+directoryName = 'data'
 nameCleanedFoler = 'cleanedJson'
 
 isArray = False
+seenIds = [] # Clean array before starting
 
 directory = os.fsencode(directoryName)
 # loop through all the files
@@ -41,21 +42,43 @@ for file in os.listdir(directory):
                                             r'\1',
                                             line)
                     # convert the TenGen JSON to Strict JSON
-                    jsondataString = re.sub(r'\:\s*\S+\s*\(\s*(\S+)\s*\)',
+                    jsondataString = re.sub(r'\:\s*[A-Z]\S+\s*\(\s*(\S+)\s*\)',
                                             r':\1',
                                             cleanedBindata)
-                    # remove any /r or /n breaks
+                    # remove any \r or \n breaks
                     removeLinebreaks = re.sub(r'\\r\\n|\\r|\\n',
                                               r'',
                                               jsondataString)
                     newLine = removeLinebreaks
-
+                
                 cleanedStringJson = cleanedStringJson + newLine
+
+
+                if ('}' in newLine) and ("{" not in newLine) and (newLine[0] == '}'):
+                    objectJsonId = json.loads(cleanedStringJson)['id']
+                    
+                    if objectJsonId in seenIds:
+                        cleanedStringJson = '' 
+                    else:
+                        seenIds.append(objectJsonId)
+
+                    # Write data to a will
+                    with open('./' + nameCleanedFoler + '/' + os.path.splitext(filename)[0] + 'Cleaned.json', 'a', encoding="utf8") as json_file:
+                        json_file.write(cleanedStringJson)
+
+                    cleanedStringJson = ''
 
                 linesCount += 1
                 progres = (linesCount/linesAmount) * 100
                 print("Progres: "+str(round(progres))+"%  Lines: " +
                       str(linesCount), end="\r", flush=True)
+
+
+                # check if this folder exist other wise make one
+                if os.path.exists(nameCleanedFoler) == False:
+                    os.mkdir(nameCleanedFoler)
+
+
 
                 # limit results to 500
                 # if (linesCount == 36000):
@@ -65,12 +88,6 @@ for file in os.listdir(directory):
             print(len(cleanedStringJson))
             # cleanedStringJson = cleanedStringJson + ']'
 
-        # check if this folder exist other wise make one
-        if os.path.exists(nameCleanedFoler) == False:
-            os.mkdir(nameCleanedFoler)
-        # Write data to a will
-        with open('./' + nameCleanedFoler + '/' + os.path.splitext(filename)[0] + 'Cleaned.json', 'w', encoding="utf8") as json_file:
-            json_file.write(cleanedStringJson)
     else:
         print("NOT JSON")
         continue
